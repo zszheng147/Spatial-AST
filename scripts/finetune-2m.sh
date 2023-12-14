cb560f05a5ada65482f251295703cd9d648fa93c
zhisheng <matrixzheng01@gmail.com>
2023-12-15 05:28:01 +0800

    logmagn, allloss

--- scripts/finetune-2m.sh
+++ scripts/finetune-2m.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export CUDA_VISIBLE_DEVICES=4,5,6,7
+export CUDA_VISIBLE_DEVICES=0,1,2,3
 export TORCH_DISTRIBUTED_DEBUG="DETAIL"
 
 blr=2e-4
@@ -9,7 +9,7 @@ mask_f_prob=0.25
 
 dataset=audioset
 # ckpt=/mnt/lustre/sjtu/home/zsz01/models/audiomae/pretrained.pth
-ckpt=/mnt/lustre/sjtu/home/zsz01/AudioMAE-fusion/outputs/finetune-2m-logmel/checkpoint-58.pth
+ckpt=/mnt/lustre/sjtu/home/zsz01/AudioMAE-fusion/outputs/finetune-2m-logmagn/checkpoint-60.pth
 
 audioset_label=/mnt/lustre/sjtu/home/zsz01/data/audioset/class_labels_indices.csv
 audioset_train_json=/mnt/lustre/sjtu/home/zsz01/data/audioset/unbalanced_no_missing.json
@@ -20,12 +20,12 @@ reverb_type=BINAURAL
 reverb_train_json=/mnt/lustre/sjtu/home/zsz01/remote/reverb/train_reverberation.json
 reverb_val_json=/mnt/lustre/sjtu/home/zsz01/remote/reverb/eval_reverberation.json
 
-output_dir=/mnt/lustre/sjtu/home/zsz01/AudioMAE-fusion/outputs/finetune-2m-logmel-allloss
-log_dir=/mnt/lustre/sjtu/home/zsz01/AudioMAE-fusion/outputs/finetune-2m-logmel-allloss
+output_dir=/mnt/lustre/sjtu/home/zsz01/AudioMAE-fusion/outputs/finetune-2m-logmagn-allloss
+log_dir=/mnt/lustre/sjtu/home/zsz01/AudioMAE-fusion/outputs/finetune-2m-logmagn-allloss
 
 # -m debugpy --listen 55555 --wait-for-client
 python -m torch.distributed.launch \
-    --nproc_per_node=4 --master_port=35352 --use_env main_finetune_as.py \
+    --nproc_per_node=4 --master_port=45352 --use_env main_finetune_as.py \
     --log_dir $log_dir \
 	--output_dir $output_dir \
     --model vit_base_patch16 \
